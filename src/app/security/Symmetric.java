3c284775600f72a18e66f8c65a0fc57336541790
Juan Alegría <juanszalegria@gmail.com>
2021-11-06 15:25:26 -0500

    Fix: read key at Symmetric

--- src/app/security/Symmetric.java
+++ src/app/security/Symmetric.java
@@ -5,8 +5,6 @@ import java.io.FileOutputStream;
 import java.security.spec.KeySpec;
 
 import javax.crypto.*;
-import javax.crypto.spec.DESKeySpec;
-import javax.crypto.spec.DESedeKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Symmetric {
@@ -82,32 +80,8 @@ public class Symmetric {
         byte[] kb = new byte[kl];
         fis.read(kb);
         fis.close();
-        KeySpec ks = null;
         SecretKey ky = null;
-        SecretKeyFactory kf = null;
-        if (algorithm.equalsIgnoreCase("DES")) {
-            ks = new DESKeySpec(kb);
-            kf = SecretKeyFactory.getInstance("DES");
-            ky = kf.generateSecret(ks);
-        } else if (algorithm.equalsIgnoreCase("DESede")) {
-            ks = new DESedeKeySpec(kb);
-            kf = SecretKeyFactory.getInstance("DESede");
-            ky = kf.generateSecret(ks);
-        } else {
-            ks = new SecretKeySpec(kb, algorithm);
-            ky = new SecretKeySpec(kb, algorithm);
-        }
-
-        /*
-         * System.out.println(); System.out.println("KeySpec Object Info: ");
-         * System.out.println("Saved File = "+fl);
-         * System.out.println("Length = "+kb.length);
-         * System.out.println("toString = "+ks.toString());
-         * 
-         * System.out.println(); System.out.println("SecretKey Object Info: ");
-         * System.out.println("Algorithm = "+ky.getAlgorithm());
-         * System.out.println("toString = "+ky.toString())
-         */
+        ky = new SecretKeySpec(kb, algorithm);
         return ky;
     }
 }
